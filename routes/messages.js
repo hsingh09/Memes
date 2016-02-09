1af6842349f3c632cf389465902716b4642b77ae
Harnoor Singh <hsingh@cs.washington.edu>
2016-02-08 21:34:07 -0800

    Close database on messages page when done

--- routes/messages.js
+++ routes/messages.js
@@ -2,16 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('data/MemeData.db');
 
 /* GET images page */
 router.get('/', function(req, res, next) {
+    var db = new sqlite3.Database('data/MemeData.db');
     var messages = [];
 
     db.each('SELECT rowid AS id, title, message FROM memes', function(err, row) {
       messages.push({title: row.title, message: row.message});
   });
     res.render('messages', { title: 'All Messages', currentPage: "messages", posts: messages});
+    db.close();
 });
 
 
