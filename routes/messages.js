7a3ac86078fd679c77f8361eaf3d298d6b42bfc1
Harnoor Singh <hsingh@cs.washington.edu>
2016-02-08 23:43:57 -0800

    push error messages to UI

--- routes/messages.js
+++ routes/messages.js
@@ -1,19 +1,31 @@
 var express = require('express');
 var router = express.Router();
-
 var sqlite3 = require('sqlite3').verbose();
+var err = "";
 
 /* GET images page */
 router.get('/', function(req, res, next) {
-    var db = new sqlite3.Database('data/MemeData.db');
+    var db = new sqlite3.Database('data/MemeData.db', function(err)
+    {
+        if (err) 
+        {
+            console.log("err");
+            this.err = err;
+        }
+    });
+
     var messages = [];
 
     db.each('SELECT rowid AS id, title, message FROM memes', function(err, row) {
-      messages.push({title: row.title, message: row.message});
+        if (err)
+        {
+            err += err;
+        }
+        messages.push({title: row.title, message: row.message});
   });
     db.close();
 
-    res.render('messages', { title: 'All Messages', currentPage: "messages", posts: messages});
+    res.render('messages', { title: 'All Messages', currentPage: "messages", posts: messages, err: err});
 });
 
 
