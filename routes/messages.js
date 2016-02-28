58c8711554ee7a2f7ac9a512c131cd23cb2a4f12
Mycry <michri@microsoft.com>
2016-02-27 17:34:49 -0800

    Added panos.jpg and simle image rendering. Set env variable memetest to use local files.

--- routes/messages.js
+++ routes/messages.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var database = require('../models/data');
+var process = require('process');
+
+var testmode = (process.env['memetest'] != undefined)
+console.log("Test mode = " + testmode)
 
 /* GET images page */
 router.get('/', function(req, res, next) {
 //    database.EnsureDB();
     var db = database.GetDB();
     var messages = [];
+    var imagehtml = '';
+    if (testmode)
+    {
+        imagehtml = '<img src="panos.jpg" style="max-width:100%;max-height:100%;">';
+    }
+    else
+    {
+        imagehtml = '<h3>TODO: Image goes here</h3>';
+    }
 
     db.each('SELECT rowid AS id, title, message FROM memes', function(err, row) {
         if (err)
@@ -14,7 +27,7 @@ router.get('/', function(req, res, next) {
             console.log("Error!");
             console.log(err);
         }
-        messages.push({title: row.title, message: row.message});
+        messages.push({title: row.title, message: row.message, image: imagehtml});
     }, function() 
     {
         res.render('messages', { title: 'All Messages', currentPage: "messages", posts: messages});
